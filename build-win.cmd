6180634cd6ad9781eef06ef31bc052070be1a9fd
Louis DeJardin <lodejard@microsoft.com>
2019-01-08 12:45:10 -0800

    build

--- build-win.cmd
+++ build-win.cmd
@@ -7,16 +7,40 @@ IF "%ARCH%" == "" SET ARCH=x64
 IF "%ARCH%" == "x64" SET MAKE_WIN="%BUILD_SOURCESDIRECTORY%\3rdparty\oniguruma\make_win64.bat"
 IF "%ARCH%" == "x86" SET MAKE_WIN="%BUILD_SOURCESDIRECTORY%\3rdparty\oniguruma\make_win32.bat"
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %ARCH%
-mkdir %BUILD_ARTIFACTSTAGINGDIRECTORY%\win-%ARCH%
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %ARCH%
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+IF NOT EXIST "%BUILD_ARTIFACTSTAGINGDIRECTORY%\win-%ARCH%" (
+    mkdir %BUILD_ARTIFACTSTAGINGDIRECTORY%\win-%ARCH%
+    if %errorlevel% neq 0 exit /b %errorlevel%
+)
+
 cd %BUILD_ARTIFACTSTAGINGDIRECTORY%\win-%ARCH%
-call %MAKE_WIN% clean
-call %MAKE_WIN% all
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+CALL %MAKE_WIN% clean
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+CALL %MAKE_WIN% all
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 cd %BUILD_SOURCESDIRECTORY%\3rdparty\oniguruma\src
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 copy %BUILD_SOURCESDIRECTORY%\3rdparty\oniguruma\windows\testc.c .
-call %MAKE_WIN% all
-call %MAKE_WIN% ctest
-call %MAKE_WIN% ptest
-call %MAKE_WIN% clean
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+CALL %MAKE_WIN% all
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+CALL %MAKE_WIN% ctest
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+CALL %MAKE_WIN% ptest
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+CALL %MAKE_WIN% clean
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 del testc.c
+if %errorlevel% neq 0 exit /b %errorlevel%
